97cfdd433bfb06cb09319af44e8031513d09d85f
DDR13GIT <ddroy13@gmail.com>
2021-12-29 12:16:22 +0600

    bottom-nav implemented with default android elements

--- app/src/main/java/com/example/busroutebd/MainActivity.java
+++ app/src/main/java/com/example/busroutebd/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.busroutebd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home_Framgent()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = item -> {
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.home:
+                selectedFragment = new Home_Framgent();
+                break;
+            case R.id.notification:
+                selectedFragment = new BusList_Framgent();
+                break;
+            case R.id.setting:
+                selectedFragment = new Settings_Framgent();
+                break;
+
+        }
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        return true;
+    };
 }
+
---
+++ app/src/main/java/com/example/busroutebd/Settings_Framgent.java
@@ -0,0 +1,19 @@
+package com.example.busroutebd;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Settings_Framgent extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        return view;
+    }
+}
